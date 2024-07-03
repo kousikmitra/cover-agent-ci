c2274d01898f1335c36cc02a18249a8a6208dffa
Kousik Mitra <kousikmitra12@gmail.com>
2024-07-03 17:42:05 +0530

    traversed changed files

--- main.py
+++ main.py
@@ -7,11 +7,26 @@ def main():
     source_dir = os.curdir
     if "INPUT_SOURCEDIR" in os.environ:
         source_dir = os.environ["INPUT_SOURCEDIR"]
-    # print(f"Executing in {source_dir} directory")
+    print(f"Executing in {source_dir} directory")
 
     # get changed files
+    files_changed = get_changed_files(source_dir)
+    print(files_changed)
+
+
+def get_changed_files(source_dir):
     repo = Repo(source_dir)
-    print(repo.head.commit.diff("main"))
+    diff = repo.head.commit.diff("main")
+
+    files_changed = []
+
+    for diff_item in diff:
+        if diff_item.a_blob and diff_item.b_blob:  # Check if the blob exists
+            file_path = diff_item.a_blob.path  # Path to the file
+            change_type = diff_item.change_type
+
+            files_changed.append((file_path, change_type))
+    return files_changed
 
 
 def set_github_action_output(output_name, output_value):
